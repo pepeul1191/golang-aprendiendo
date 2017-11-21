9b822c8d050a1a37142aa4dced44fe8efa12a5c4
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-21 10:11:44 -0500

    Operadores lógicos

--- tuto1.go
+++ tuto1.go
@@ -15,4 +15,18 @@ func main(){
 	fmt.Println("6 * 4 = ", 6 * 4)
 	fmt.Println("6 / 4 = ", 6 / 4)
 	fmt.Println("6 % 4 = ", 6 % 4)
+	fmt.Println("++++++++++++++++++++++++++++++++++++")
+	const pi float64 = 3.14159265
+	var miNombre = "Pepe Valdivia"
+	fmt.Println(miNombre, " - n = ", len(miNombre))
+	fmt.Println("++++++++++++++++++++++++++++++++++++")
+	fmt.Println("true && false = ", true && false)
+	fmt.Println("true || false = ", true || false)
+	fmt.Println("!true = ", !true)
+	fmt.Println("++++++++++++++++++++++++++++++++++++")
+	var i int = 1
+	for (i <= 10) {
+		fmt.Println(i)
+		i = i + 1
+	}
 }
