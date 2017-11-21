5e436833b93782ac668966f3a6a386b6a8215ddc
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-21 09:53:27 -0500

    Operaciones aritméticas

--- tuto1.go
+++ tuto1.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main(){
 	fmt.Println("hola mundo")
+	var edad int = 29
+	var peso float32 = 88.3  
+	cualquier := "1"
+	fmt.Println(cualquier)
+	fmt.Println("Edad : ", edad, " - Peso : ", peso)
+	fmt.Println("++++++++++++++++++++++++++++++++++++")
+	fmt.Println("6 - 4 = ", 6 - 4)
+	fmt.Println("6 + 4 = ", 6 + 4)
+	fmt.Println("6 * 4 = ", 6 * 4)
+	fmt.Println("6 / 4 = ", 6 / 4)
+	fmt.Println("6 % 4 = ", 6 % 4)
 }
